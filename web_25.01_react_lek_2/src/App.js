a47552e8b018c18e2cec6066ca0665ac7c65b8cd
AgaDra <agatymail@gmail.com>
2024-02-06 13:14:01 +0100

    lekcja2

---
+++ web_25.01_react_lek_2/src/App.js
@@ -0,0 +1,13 @@
+import style from "./App.module.scss";
+import Form from "./components/Form/Form";
+
+function App() {
+  return (
+    <div className="container">
+      <section className={style.section}></section>
+      <h2> Wprowadź swoje dane:</h2>
+    </div>
+  );
+}
+
+export default App;
